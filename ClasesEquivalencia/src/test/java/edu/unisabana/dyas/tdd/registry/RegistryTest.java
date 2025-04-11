db711387ad9b3fbfb0fc793d5538784913118c64
davidmedina0526 <davidmebu@unisabana.edu.co>
2025-04-11 10:16:32 -0500

    Skeleton

---
+++ ClasesEquivalencia/src/test/java/edu/unisabana/dyas/tdd/registry/RegistryTest.java
@@ -0,0 +1,15 @@
+package edu.unisabana.dyas.tdd.registry;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class RegistryTest {
+    private Registry registry = new Registry();
+    @Test
+    public void validateRegistryResult() {
+        Person person = new Person();
+        RegisterResult result = registry.registerVoter(person);
+        Assert.assertEquals(RegisterResult.VALID, result);
+    }
+    // TODO Complete with more test cases
+}
